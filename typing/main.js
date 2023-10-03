373ee144633a9ab5a1fa1fc08d2fa4b802e46984
touki1223 <141597234+touki1223@users.noreply.github.com>
2023-10-03 09:48:33 +0900

    Add files via upload

--- typing/main.js
+++ typing/main.js
@@ -2,10 +2,10 @@
 let untyped = '';
 let typed = '';
 let score = 0;
- 
+
 // 必要なHTML要素の取得
 const untypedfield = document.getElementById('untyped');
-const typedfield =document.getElementById('typed');
+const typedfield = document.getElementById('typed');
 const wrap = document.getElementById('wrap');
 const start = document.getElementById('start');
 const count = document.getElementById('count');
@@ -13,18 +13,18 @@ const count = document.getElementById('count');
 // 複数のテキストを格納する配列
 const textLists = [
   'Hello World','This is my App','How are you?',
-   'Today is sunny','I love JavaScript!','Good morning',
-   'I am Japanese','Let it be','Samurai',
-   'Typing Game','Information Technology',
-   'I want to be a programmer','What day is today?',
-   'I want to build a web app','Nice to meet you',
-   'Chrome Firefox Edge Safari','machine learning',
-   'Brendan Eich','John Resig','React Vue Angular',
-   'Netscape Communications','undefined null NaN',
-   'Thank you very much','Google Apple Facebook Amazon',
-   'ECMAScript','console.log','for while if switch',
-   'var let const','Windows Mac Linux iOS Android',
-   'programming'
+  'Today is sunny','I love JavaScript!','Good morning',
+  'I am Japanese','Let it be','Samurai',
+  'Typing Game','Information Technology',
+  'I want to be a programmer','What day is today?',
+  'I want to build a web app','Nice to meet you',
+  'Chrome Firefox Edge Safari','machine learning',
+  'Brendan Eich','John Resig','React Vue Angular',
+  'Netscape Communications','undefined null NaN',
+  'Thank you very much','Google Apple Facebook Amazon',
+  'ECMAScript','console.log','for while if switch',
+  'var let const','Windows Mac Linux iOS Android',
+  'programming'
 ];
 
 // ランダムなテキストを表示
@@ -33,9 +33,9 @@ const createText = () => {
   // 正タイプした文字列をクリア
   typed = '';
   typedfield.textContent = typed;
-  
+
   // 配列のインデックス数からランダムな数値を生成する
-  let random = Math.floor(Math.random() * textLists.length);
+  let random= Math.floor(Math.random() * textLists.length);
 
   // 配列からランダムにテキストを取得し画面に表示する
   untyped = textLists[random];
@@ -46,20 +46,19 @@ const createText = () => {
 const keyPress = e => {
 
   // 誤タイプの場合
-  if(e.key !== untyped.substring(0,1)) {
+  if(e.key !== untyped.substring(0, 1)) {
     wrap.classList.add('mistyped');
     // 100ms後に背景色を元に戻す
     setTimeout(() => {
       wrap.classList.remove('mistyped');
-    },100);
+    }, 100);
     return;
   }
 
   // 正タイプの場合
   // スコアのインクリメント
   score++;
-  wrap.classList.remove('mistyped');
-  typed += untyped.substring(0,1);
+  typed += untyped.substring(0, 1);
   untyped = untyped.substring(1);
   typedfield.textContent = typed;
   untypedfield.textContent = untyped;
@@ -75,20 +74,20 @@ const rankCheck = score => {
 
   // テキストを格納する変数を作る
   let text = '';
-
-// スコアに応じて異なるメッセージを変数textに格納する
-if(score < 100) {
-  text = `あなたのランクはCです。\nBランクまであと${100 - score}文字です。`;
-} else if(score < 200) {
-  text = `あなたのランクはBです。\nAランクまであと${200 - score}文字です。`;
-} else if(score < 300) {
-  text = `あなたのランクはAです。\nSランクまであと${300 - score}文字です。`;
-} else if(score >= 300) {
-  text = `あなたのランクはSです。\nおめでとうございます!`;
-}
-
-// 生成したメッセージと一緒に文字列を返す
-return `${score}文字打てました!\n${text}\n${text}\n 【OK】リトライ / 【キャンセル】終了`;
+ 
+  // スコアに応じて異なるメッセージを変数textに格納する
+  if(score < 100) {
+    text = `あなたのランクはCです。\nBランクまであと${100 - score}文字です。`;
+  } else if(score < 200) {
+    text = `あなたのランクはBです。\nAランクまであと${200 - score}文字です。`;    
+  } else if(score < 300) {
+    text = `あなたのランクはAです。\nSランクまであと${300 - score}文字です。`;    
+  } else if(score >= 300) {
+    text = `あなたのランクはSです。\nおめでとうございます!`;    
+  }
+ 
+  // 生成したメッセージと一緒に文字列を返す
+  return `${score}文字打てました!\n${text}\n【OK】リトライ / 【キャンセル】終了`;
 };
 
 // ゲームを終了
@@ -106,7 +105,7 @@ const gameOver = id => {
 // カウントダウンタイマー
 const timer = () => {
 
-  // タイマー部分のHTML要素 (p要素) を取得する
+  // タイマー部分のHTML要素（p要素）を取得する
   let time = count.textContent;
 
   const id = setInterval(() => {
@@ -119,7 +118,7 @@ const timer = () => {
     if(time <= 0) {
       gameOver(id);
     }
-  },1000);
+  }, 1000);
 };
 
 // ゲームスタート時の処理
